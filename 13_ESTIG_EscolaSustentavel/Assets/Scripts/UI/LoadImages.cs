362dba6eed80637676a7ee6664d96631400691b2
Rodrigo Fernandes <rfernandes@digital-works.com>
2021-02-20 17:34:39 +0000

    Added empty load images script

---
+++ 13_ESTIG_EscolaSustentavel/Assets/Scripts/UI/LoadImages.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Database;
+using UnityEngine;
+
+public class LoadImages : MonoBehaviour
+{
+    private DatabaseManager _databaseManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _databaseManager = GameObject.FindGameObjectWithTag("UiCanvas").GetComponent<Game>().databaseManager;
+    }
+
+    private void LoadImagesPanels()
+    {
+        
+    }
+    
+    private void LoadImagesLamps()
+    {
+        
+    }
+    
+    private void LoadImagesSensors()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
