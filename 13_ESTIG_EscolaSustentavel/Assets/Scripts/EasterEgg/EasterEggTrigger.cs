ecc0ee6ad9e452519d6e5710081238b60cf1aaec
Rodrigo Fernandes <rfernandes@digital-works.com>
2021-02-25 03:29:43 +0000

    Added easteregg

---
+++ 13_ESTIG_EscolaSustentavel/Assets/Scripts/EasterEgg/EasterEggTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggTrigger : MonoBehaviour
+{
+    public AudioSource sound;
+    public GameObject wall;
+    public Material image;
+    public Material defaultMaterial;
+    private bool _entered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _entered = !_entered;
+        if (sound.isPlaying && !_entered)
+        {
+            sound.Stop();
+            wall.GetComponent<Renderer>().material = defaultMaterial;
+        }
+        else if(_entered)
+        {
+            sound.Play();
+            wall.GetComponent<Renderer>().material = image;
+        }
+    }
+}
